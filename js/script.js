5e70747787ebbfc32294049b3f89e58e391fbad0
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-11-04 16:47:35 +0100

    HTML + CSS + JS PRINT ICONE

--- js/script.js
+++ js/script.js
@@ -103,9 +103,37 @@ $(document).ready(function () {
     ];
     
     // REFERENZE HTML
-    // JQ per targhettizzare il container icons
+    // Sintassi JQ per targhettizzare il container icons in cui inietteremo le icone
+    const container = $('.icons');
 
+    // PRINT ICONE SU SCHERMO
+    // I due attributi della funzione che invoco per printare le icone su schermo corrispondono all'array da cui prendere le icone e alla var const in cui iniettarle
+    printIcons(icons, container);
 
 
 
 }); // End document ready
+
+
+// *********************************************
+// FUNCTIONS
+// *********************************************
+
+// FUNZIONE PER PRINTARE LE ICONE SU SCHERMO
+function printIcons(icons, container) {
+    // Devo iterare su tutte le icone presenti nell'array, il metodo migliore consiste nell'utilizzare .FOREACH sull'array icons. Icon è la name convention più comprensibile e sostituisce l'element della sintassi base.
+    icons.forEach(( icon ) => {
+        // Destructuring dell'oggetto nell'array al fine di estrapolare da esso solo i valori che mi servono con una sintassi breve e comprensibile
+        const {family, prefix, name} = icon;
+        // Creo una var const con ciò che vado ad iniettare effettivamente nell'HTML utilizzando il literal template (interpolazione di stringhe) per inserire dinamicamente i dati sostituendo i valori fissi di ogni icona con i dati che di volta in volta itero grazie al .FOREACH e che ho destrutturato in precedenza 
+        const html = 
+            `<div class="icon">
+                <i class="${family} ${prefix}${name}"></i>
+                <div class="icon-name">
+                    ${name}
+                </div>
+            </div>`;
+            // Con .append 'attacco' al mio container i dati che ho estrapolato ed inietto il tutto nel mio HTML
+            container.append(html);
+    });
+};
